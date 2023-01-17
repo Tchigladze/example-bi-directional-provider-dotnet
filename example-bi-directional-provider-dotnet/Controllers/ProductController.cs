7c19cee113cd8276185562a960a32a90b0457bf3
Tchigladze <104762501+Tchigladze@users.noreply.github.com>
2023-01-17 12:55:56 +0400

    sds

--- example-bi-directional-provider-dotnet/Controllers/ProductController.cs
+++ example-bi-directional-provider-dotnet/Controllers/ProductController.cs
@@ -40,21 +40,5 @@ namespace Provider.Controllers
 
             return product;
         }
-
-        // GET /products/{id}
-        [HttpGet("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<Product> GetProduct2(int id)
-        {
-            var product = products.FirstOrDefault(product => product.id == id);
-
-            if (product == null)
-            {
-                return new NotFoundResult();
-            }
-
-            return product;
-        }
     }
 }
