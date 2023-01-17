bf51f05cefc72c5a70c14a0eff41692ea2275f17
Tchigladze <104762501+Tchigladze@users.noreply.github.com>
2023-01-17 12:24:15 +0400

    update contract

--- example-bi-directional-provider-dotnet/Models/Product.cs
+++ example-bi-directional-provider-dotnet/Models/Product.cs
@@ -5,6 +5,6 @@
         public int id { get; set; }
         public string type { get; set; }
         public string name { get; set; }
-        public long name1 { get; set; }
+        public long Weight { get; set; }
     }
 }
