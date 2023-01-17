216ad58cdfc1d23cce42bfc6247da97fe69705a5
Tchigladze <104762501+Tchigladze@users.noreply.github.com>
2023-01-17 17:38:37 +0400

    update contract

--- example-bi-directional-provider-dotnet/Controllers/ProductController.cs
+++ example-bi-directional-provider-dotnet/Controllers/ProductController.cs
@@ -40,5 +40,20 @@ namespace Provider.Controllers
 
             return product;
         }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Product> GetProduct2(int id)
+        {
+            var product = products.FirstOrDefault(product => product.id == id);
+
+            if (product == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return product;
+        }
     }
 }
